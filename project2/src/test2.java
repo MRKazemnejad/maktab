a47a63bd9d4ac22a3e30973fe6b13ec05416f3e2
MRKazemnejad <mreza.kazemnejad@gmail.com>
2019-03-14 20:25:41 +0330

    line saeed added

--- project2/src/test2.java
+++ project2/src/test2.java
@@ -2,5 +2,6 @@ public class test2 {
     public static void main(String[] args) {
         System.out.println("hallo again");
         System.out.println("reza");
+        System.out.println("saeed");
     }
 }
