71f7d4be83c6252a9c5fc0a946430bb1700e5614
MRKazemnejad <mreza.kazemnejad@gmail.com>
2019-03-14 20:15:18 +0330

    final project

--- project1/src/test1.java
+++ project1/src/test1.java
@@ -2,13 +2,11 @@ public class test1 {
     public static void main(String[] args) {
         System.out.println("hello reza");
         System.out.println("khobi");
-<<<<<<< HEAD
         System.out.println("by");
-=======
         System.out.println("line 1");
         System.out.println("line 2");
         System.out.println("line 3");
->>>>>>> develop_p1
+
     }
 
 }
