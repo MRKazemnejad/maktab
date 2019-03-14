4aaa581e5e32e46b99af93a1b4b6de5645246e51
MRKazemnejad <mreza.kazemnejad@gmail.com>
2019-03-14 20:26:09 +0330

    line goodby added

--- project2/src/test2.java
+++ project2/src/test2.java
@@ -3,5 +3,6 @@ public class test2 {
         System.out.println("hallo again");
         System.out.println("reza");
         System.out.println("saeed");
+        System.out.println("goodby");
     }
 }
